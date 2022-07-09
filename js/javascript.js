820e5e2c3653f381e51c9fcd3f45c638e5ededcd
Sahil chaudhari <sahilchaudhari2405@gmail.com>
2022-07-10 02:57:20 +0530

    without responsive page finish

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+/* ================================== typing animation==================================== */
+// var typed = new Typed(".typing",{
+//     Strings:['Web Designer','Competitive Programmer','Web Devloper'],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// })
+var typed2 = new Typed('.typing', {
+    strings: ['Web Designer','Web Devloper', 'Competitive Programmer'],
+    typeSpeed: 70,
+    backSpeed: 60,
+    smartBackspace: true,
+    fadeOut: true,
+    loop: true
+  });
